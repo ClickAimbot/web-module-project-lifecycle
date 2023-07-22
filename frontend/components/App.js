ffb272b06f8a1f8e08a9dcc9e1aa07f40066fa37
Austin Amaral <austin.amaral3@gmail.com>
2023-07-22 13:29:26 -0700

    rendering all todos from api server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,43 @@
 import React from 'react'
-
+import axios from 'axios';
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({ ...this.state, todos: res.data.data })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  componentDidMount() {
+    console.log('component did mount');
+    this.fetchAllTodos()
+  }
+
+  componentDidUpdate() {
+    console.log('component did update')
+  }
+
   render() {
-    return null
+    return (
+      <>
+        <h1>Things need taking care of!</h1>
+        <h2>Todos</h2>
+        {
+          this.state.todos.map(todo => {
+            return <div key={todo.id}>{todo.name}</div>
+          })
+        }
+      </>
+    )
   }
 }
